53f230a61c95755427edc9f6102f9ba54e953a9c
chingmaldo <ching.maldo@yahoo.com>
2024-08-10 06:01:48 +0000

    form04 & database04

--- public_html/database04.php
+++ public_html/database04.php
@@ -15,13 +15,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname, email, reg_date FROM MyGuests";
+$sql = "SELECT id, name, email, website, gender, reg_date FROM MyForm";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . " " . $row["email"] . " " . $row["reg_date"] . "<br>";
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["email"] . " " . $row["website"] . " " . $row["gender"] ." " . $row["reg_date"] . "<br>";
     }
 } else {
     echo "0 results";
