8f9cea6e00e705f2900940223b476ce4519cf89a
chingmaldo <ching.maldo@yahoo.com>
2024-08-17 01:37:33 +0000

    Commit 17Aug24

--- public_html/ci4/app/Views/news/create.php
+++ public_html/ci4/app/Views/news/create.php
@@ -10,6 +10,10 @@
     <input type="input" name="title" value="<?= set_value('title') ?>">
     <br>
 
+    <label for="title">Author</label>
+    <input type="input" name="title" value="<?= set_value('author') ?>">
+    <br>
+
     <label for="body">Text</label>
     <textarea name="body" cols="45" rows="4"><?= set_value('body') ?></textarea>
     <br>
